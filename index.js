678d3d2621d73699000e600d51dc4c18a248ad5b
aekiratli <52128960+aekiratli@users.noreply.github.com>
2021-10-12 21:01:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const Web3 = require('web3')
+let web3 = new Web3(
+    new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/ws/v3/5abfb0d0480e43049a0fce761328a78f")
+  );
+
+const port = 3000;
+const address = "0x091822f59d65a35bcdeb015135af37a7a8f1b3cd"
+const abi = require('./abi.json')
+
+app.get('/', async (req, res) => {
+    
+    const instance = new web3.eth.Contract(abi, address);
+    const test = await instance.methods.totalSupply().call()
+    console.log(test)
+    res.send(test);
+});
+
+app.listen(port, () => {
+   console.log(`Server is up at ${port}`);
+});
