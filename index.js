642d961d948d5c79bbe86f3eb58c4fcf9043f6fe
aekiratli <emre719@gmail.com>
2021-12-28 21:58:15 +0300

    branch

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const express = require('express');
 const app = express();
 const Web3 = require('web3')
 const PORT = process.env.PORT || 3000;
-let web3 = new Web3(
-    new Web3.providers.WebsocketProvider("wss://rinkeby.infura.io/ws/v3/5abfb0d0480e43049a0fce761328a78f")
-  );
+ let web3 = new Web3(
+     new Web3.providers.HttpProvider("https://data-seed-prebsc-2-s3.binance.org:8545/")
+   );
 
-const address = "0x2C0CF5DF938f71e0887AB9cB32B037FD1d505B7B"
-const abi = require('./abi.json')
+ const address = "0xcdb319059e5e5b8c648b627bf487587e5bf16a86"
+ const abi = require('./abi.json')
 app.get('/', async (req, res) => {
     
-    const instance = new web3.eth.Contract(abi, address);
-    const supply = await instance.methods.totalSupply().call()
-    res.send(supply);
+     const instance = new web3.eth.Contract(abi, address);
+     const supply = await instance.methods.totalSupply().call()
+     
+     res.send(supply);
+    
 });
 
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
+//  app.listen(3000, () => {
+//      console.log(`Our app is running on port 3000`);
+//  });
